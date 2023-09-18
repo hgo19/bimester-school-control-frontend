ba7a1feed01b4fcd8d6fa83139749a37e85a2313
Hugo Leonardo <huleonardo19@gmail.com>
2023-09-18 17:40:52 -0300

    feat: create media query for bimester-header

--- src/components/bimester-header/index.tsx
+++ src/components/bimester-header/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import plusSvg from '../../assets/Plus.svg'
 import './styles.css'
 import { BimesterHeaderTypes } from './types'
@@ -9,6 +9,21 @@ import Tooltip from '../tooltip'
 export default function BimesterHeader({ bimestre }: BimesterHeaderTypes) {
   const [showModal, setShowModal] = useState(false)
 
+  const [isTextVisible, setIsTextVisible] = useState(window.innerWidth >= 900)
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsTextVisible(window.innerWidth >= 970)
+    }
+
+    window.addEventListener('resize', handleResize)
+    handleResize() // Inicialize com o valor correto
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
   const handleClick = () => {
     setShowModal(true)
   }
@@ -18,7 +33,8 @@ export default function BimesterHeader({ bimestre }: BimesterHeaderTypes) {
       <h3 className="bimester-header-title">{bimestre}</h3>
       <Tooltip delay={0} direction={'top'} content={'Adicionar'}>
         <button type="button" onClick={handleClick} className="add-button">
-          <img src={plusSvg} alt="plus-svg" />
+          {isTextVisible && <p className="button-text">Lançar Nota</p>}
+          <img src={plusSvg} alt="plus-svg" className="plus-svg" />
         </button>
       </Tooltip>
       {showModal &&
